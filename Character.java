ddacd1071b557e3343e954a0e6fdcd03e576dd9e
1noseA <1noseA2L@gmail.com>
2020-08-11 14:18:25 +0900

    [11-3]抽象クラス・抽象メソッド

---
+++ Character.java
@@ -0,0 +1,15 @@
+// abstractメソッドを含むクラスはabstractをつける（抽象クラス）
+// 抽象クラスはnewによるインスタンス化が禁止される
+// 子クラスでオーバーライドしないとエラーになる
+public abstract class Character {
+  String name;
+  int hp;
+
+  public void run() {
+    System.out.println(this.name + "は逃げ出した");
+  }
+
+  // abstract抽象メソッド
+  public abstract void attack(Matango m);
+
+}
