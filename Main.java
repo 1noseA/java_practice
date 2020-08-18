a3c46a9c3c77b598800c4eac26410a444f33274c
1noseA <1noseA2L@gmail.com>
2020-08-18 10:38:35 +0900

    [9-4-2]例外処理

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 import java.io.*;
 public class Main {
-  public static void main(String[] args) throws IOException {
-    FileOutputStream fos = new FileOutputStream("rpgsave.dat", true);
-    fos.write(65);
-    fos.flush();
-    fos.close();
+  public static void main(String[] args) {
+    // tryブロックの外で宣言し、nullで初期化しないと
+    // finallyブロック内でclose()を呼べない
+    FileWriter fw = null;
+    try {
+      fw = new FileWriter("rpgsave.dat", true);
+      fw.write('A');
+      fw.flush();
+    } catch(IOException e) {
+      System.out.println("ファイル書き込みエラーです");
+      // ファイルを閉じるためのfinallyブロック
+    } finally {
+      if (fw != null) {
+        try {
+          fw.close();
+          // 失敗しても何もできないためcatchブロックの中は空
+        } catch(IOException e2) { }
+      }
+    }
   }
 }
