a690b4334fc78b1202053fb4df860ce1b1a8337e
1noseA <1noseA2L@gmail.com>
2020-08-18 14:15:55 +0900

    [10-4]XML

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 import java.io.*;
-import java.util.*;
 public class Main {
   public static void main(String[] args) throws Exception {
-    Reader fr = new FileReader("c:\\rpgdata.properties");
-    Properties p = new Properties();
-    // ファイルの内容を読み込む
-    p.load(fr);
-    // キーを指定し値を取り出す
-    String name = p.getProperty("heroName");
-    String strHp = p.getProperty("heroHp");
-    int hp = Integer.parseInt(strHp);
-    System.out.println("勇者の名前：" + name);
-    System.out.println("勇者のHP；" + hp);
-    fr.close();
+    InputStream is = new FileInputStream("c:\\rpgsave.xml");
+    Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
+    Element hero = doc.getDocumentElement();
+    Element weapon = findChildByTag(hero, "weapon");
+    Element power = findChildByTag(weapon, "power");
+    String value = power.getTextContent();
+  }
+  // 指定された名前を持つタグの最初の子タグを返す
+  static Element findChildByTag(Element self, String name) throws Exception {
+    // すべての子を取得
+    NodeList children = self.getChildNodes();
+    for (int i = 0; i < children.getLength(); i++) {
+      if (children.item(i) instanceof Element) {
+        Element e = (Element) children.item(i);
+        // タグ名を照合
+        if (e.getTagName().equals(name)) {
+          return e;
+        }
+      }
+    }
+    return null;
   }
 }
