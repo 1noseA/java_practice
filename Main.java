2c288b584770c3d4dd8e5cd6c15dbf62e796642e
1noseA <1noseA2L@gmail.com>
2020-08-18 10:18:10 +0900

    [9-3-2]FileOutputStream

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 import java.io.*;
 public class Main {
   public static void main(String[] args) throws IOException {
-    FileReader fw = new FileReader("rpgsave.dat");
-    System.out.println("すべてのデータを読んで表示します");
-    int i = fw.read();
-    while(i != -1) {
-      char c = (char) i;
-      System.out.println(c);
-      i = fw.read();
-    }
-    System.out.println("ファイルの末尾に到達しました");
-    fw.close();
+    FileOutputStream fos = new FileOutputStream("rpgsave.dat", true);
+    fos.write(65);
+    fos.flush();
+    fos.close();
   }
 }
