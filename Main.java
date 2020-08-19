e54f61c8d1bbc3b56485f1d7be2a61ae408531bd
1noseA <1noseA2L@gmail.com>
2020-08-19 11:47:21 +0900

    [11-3]ServerSocket

--- Main.java
+++ Main.java
@@ -1,20 +1,12 @@
-import java.io.*;
 import java.net.*;
 public class Main {
-  // 例外処理を省略(本来は書く)
-  public static void main(String[] args) throws IOException {
-    Socket sock = new Socket("dokojava.jp", 80);
-    InputStream is =sock.getInputStream();
-    OutputStream os = sock.getOutputStream();
-    os.write("GET /index.html HTTP/1.0\r\n".getBytes());
-    os.write("\r\n".getBytes());
-    os.flush();
-    InputStreamReader isr = new InputStreamReader(is);
-    int i = isr.read();
-    while (i != -1) {
-      System.out.print((char) i);
-      i = isr.read();
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("起動完了");
+    ServerSocket svSock = new ServerSocket(39648);
+    Socket sock = svSock.accept();
+    System.out.println(sock.getInetAddress() + "から接続");
+    sock.getOutputStream().write("WELCOME".getBytes());
+    sock.getOutputStream().flush();
     sock.close();
   }
 }
