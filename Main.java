e56ec7bbe117154918f8beea5f5e1c5c2a974b98
1noseA <1noseA2L@gmail.com>
2020-08-19 11:39:55 +0900

    [11-2]Socket

--- Main.java
+++ Main.java
@@ -3,14 +3,18 @@ import java.net.*;
 public class Main {
   // 例外処理を省略(本来は書く)
   public static void main(String[] args) throws IOException {
-    URL url = new URL("http://dokojava.jp");
-    InputStream is = url.openStream();
+    Socket sock = new Socket("dokojava.jp", 80);
+    InputStream is =sock.getInputStream();
+    OutputStream os = sock.getOutputStream();
+    os.write("GET /index.html HTTP/1.0\r\n".getBytes());
+    os.write("\r\n".getBytes());
+    os.flush();
     InputStreamReader isr = new InputStreamReader(is);
     int i = isr.read();
     while (i != -1) {
       System.out.print((char) i);
       i = isr.read();
     }
-    isr.close();
+    sock.close();
   }
 }
